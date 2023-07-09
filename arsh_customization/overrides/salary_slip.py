9077178b0830b461904dd5834e3bbad9cf166ec7
Abdussamad Bello <abdussamadbello4real@hotmail.co.uk>
2023-07-09 19:49:08 +0100

    added get_field_days

--- arsh_customization/overrides/salary_slip.py
+++ arsh_customization/overrides/salary_slip.py
@@ -4,31 +4,73 @@ import frappe
 
 
 class TotalExemptionAmount(SalarySlip):
-	def get_total_exemption_amount(self):
-		total_exemption_amount = 0
-		taxable_income =0
-
-		if self.tax_slab.allow_tax_exemption:
-			if self.deduct_tax_for_unsubmitted_tax_exemption_proof:
-				exemption_proof = frappe.db.get_value(
-					"Employee Tax Exemption Proof Submission",
-					{"employee": self.employee, "payroll_period": self.payroll_period.name, "docstatus": 1},
-					["exemption_amount"],
-				)
-				if exemption_proof:
-					total_exemption_amount = exemption_proof
-			else:
-				declaration = frappe.db.get_value(
-					"Employee Tax Exemption Declaration",
-					{"employee": self.employee, "payroll_period": self.payroll_period.name, "docstatus": 1},
-					["total_exemption_amount"],
-				)
-				if declaration:
-					total_exemption_amount = declaration
-			for earning in self.earnings:
-				if earning.is_tax_applicable:
-					taxable_income += earning.amount
-			total_exemption_amount += max(0.21 * taxable_income, 16666.67+0.2*taxable_income)
-		return total_exemption_amount
 	
+    def before_validate(self):
+             self.get_field_days()
+             
+    
+    def get_total_exemption_amount(self, payroll_period, tax_slab):
+        total_exemption_amount = 0
+        taxable_income = 0
 
+        if tax_slab.allow_tax_exemption:
+            if self.deduct_tax_for_unsubmitted_tax_exemption_proof:
+                exemption_proof = frappe.db.get_value(
+                    "Employee Tax Exemption Proof Submission",
+                    {
+                        "employee": self.employee,
+                        "payroll_period": payroll_period.name,
+                        "docstatus": 1,
+                    },
+                    ["exemption_amount"],
+                )
+                if exemption_proof:
+                    total_exemption_amount = exemption_proof
+            else:
+                declaration = frappe.db.get_value(
+                    "Employee Tax Exemption Declaration",
+                    {
+                        "employee": self.employee,
+                        "payroll_period": payroll_period.name,
+                        "docstatus": 1,
+                    },
+                    ["total_exemption_amount"],
+                )
+                if declaration:
+                    total_exemption_amount = declaration
+
+            # Calculate taxable Income -  Ysf modification
+            for earning in self.earnings:
+                if earning.is_tax_applicable:
+                    taxable_income += earning.amount
+            total_exemption_amount += max(
+                0.21 * taxable_income, 16666.67 + 0.2 * taxable_income
+            )
+
+        return total_exemption_amount
+
+    @frappe.whitelist()
+    def get_field_days(self):
+        # get onshore days from attendance
+        self.onshore_days = len(
+            frappe.get_all(
+                "Attendance",
+                filters={
+                    "attendance_date": ["between", [self.start_date, self.end_date]],
+                    "field_days": "onshore",
+                    "employee": self.employee,
+                    "docstatus": 1,
+                },
+            )
+        )
+        self.offshore_days = len(
+            frappe.get_all(
+                "Attendance",
+                filters={
+                    "attendance_date": ["between", [self.start_date, self.end_date]],
+                    "field_days": "offshore",
+                    "employee": self.employee,
+                    "docstatus": 1,
+                },
+            )
+        )
