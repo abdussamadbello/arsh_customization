6ba9a541fea59829666858f9172da79d034de5c0
Abdussamad Bello <abdussamadbello4real@hotmail.co.uk>
2024-09-21 19:27:38 +0000

    Refactor arsh_customization bundle.js

---
+++ arsh_customization/public/arsh_customization.bundle.js
@@ -0,0 +1,4 @@
+import './js/employee_advance';
+import './js/payment_entry';
+import './js/timesheet';
+import './templates/includes/timesheet_warnings.html'
--- arsh_customization/public/js/arsh_customization.bundle.js
+++
@@ -1,4 +0,0 @@
-import './employee_advance';
-import './payment_entry';
-import './timesheet';
-import '../templates/includes/timesheet_warnings.html'
